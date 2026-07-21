0fe4c5dc7d426c5658f10eb89d5f933d272278e2
Arif Shaon <ashaon@qf.org.qa>
2026-07-21 13:37:05 +0300

    Add placemark name selection control code-behind

---
+++ KmlScopedEditor/Views/PlacemarkNameSelectionControl.xaml.cs
@@ -0,0 +1,9 @@
+namespace KmlScopedEditor.Views;
+
+public partial class PlacemarkNameSelectionControl : System.Windows.Controls.UserControl
+{
+    public PlacemarkNameSelectionControl()
+    {
+        InitializeComponent();
+    }
+}
